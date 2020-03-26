1fe104dd12ceff16503a0e87c5eb7bcadbd3b57f
rizzok <kevin.rizzo@live.fr>
2020-03-26 22:58:34 +0100

    page Projets : ajout des sites que j'ai créés

--- pages/projets.php
+++ pages/projets.php
@@ -16,13 +16,85 @@
         </div>
 
 		<div class="container">
+
 			<div class="row">
 				<div class="col-lg-9 m-auto">
 
-
+                    <div class="my-5 text-center">
+                        <h2>Les sites web que j'ai créés</h2>
+                    </div>
 
                 </div>
             </div>
+
+
+            <div class="row row-cols-1 row-cols-md-2 text-center mb-5">
+                <div class="col mb-4">
+                    <div class="card">
+                        <div>
+                            <img src="/img/projets/ssb-b2c.jpg" class="d-block m-auto py-4" alt="SSB - B2C">
+                        </div>
+                        <div class="card-body border-top">
+                            <p class="h5 card-title">Sur le Sentier des Bergers - B2C</p>
+                            <p class="card-text">Épicerie fine en ligne destinée aux particuliers.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+                            <div class="mb-4 m-auto text-center">
+                                <a href="https://www.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+                                    <button type="button" class="btn btn-primary">Voir ce site</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+                        <div>
+                            <img src="/img/projets/ssb-b2b.jpg" class="d-block m-auto py-2" alt="SSB - B2B" width="174">
+                        </div>
+                        <div class="card-body border-top">
+                            <p class="h5 card-title">Sur le Sentier des Bergers - B2B</p>
+                            <p class="card-text">Épicerie fine en ligne destinée aux professionnels.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+                            <div class="mb-4 m-auto text-center">
+                                <a href="https://pro.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+                                    <button type="button" class="btn btn-primary">Voir ce site</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+                        <div>
+                            <img src="/img/projets/bj.jpg" class="d-block m-auto py-4" alt="BJ">
+                        </div>
+                        <div class="card-body border-top">
+                            <p class="h5 card-title">La Boutique des Jambes</p>
+                            <p class="card-text">Vente de produits de bien-être pour les jambes.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+                            <div class="mb-4 m-auto text-center">
+                                <a href="https://www.laboutiquedesjambes.com/" target="_blank" rel="noopener noreferrer">
+                                    <button type="button" class="btn btn-primary">Voir ce site</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+                        <div>
+                            <img src="/img/projets/trcom.png" class="d-block m-auto py-4" alt="TR com">
+                        </div>
+                        <div class="card-body border-top">
+                            <p class="h5 card-title">TR Communication</p>
+                            <p class="card-text">Community Management, Imprimerie & Signalétique<br>Site vitrine créé avec le CMS Wordpress.</p>
+                            <div class="mb-4 m-auto text-center">
+                                <a href="https://tr-communication.fr/" target="_blank" rel="noopener noreferrer">
+                                    <button type="button" class="btn btn-primary">Voir ce site</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
 <?php require '../inc/footer.php';?>
