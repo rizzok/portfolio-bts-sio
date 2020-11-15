7bcbf5694383abaaa684c1b257212e652038f41b
rizzokdev <kevinrizzo13100@gmail.com>
2020-11-15 17:37:44 +0100

    Update to simple page

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151636424-2"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VYSBJK2XKF"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -11,27 +11,10 @@
             dataLayer.push(arguments);
         }
         gtag('js', new Date());
-        gtag('config', 'UA-151636424-2');
+
+        gtag('config', 'G-VYSBJK2XKF');
     </script>
     <!-- End Google Analytics -->
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-MW4WZ9N');
-    </script>
-    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?= $page_description ?? 'Kévin RIZZO - Étudiant en BTS SIO SLAM' ?>">
@@ -48,58 +31,9 @@
     <link href="/css/style.css" rel="stylesheet">
     <!-- Font awesome -->
     <link href="/css/fontawesome/css/all.css" rel="stylesheet">
-    <!-- <script src="https://kit.fontawesome.com/fafd7398ca.js" crossorigin="anonymous"></script> -->
 </head>
 
 <body class="d-flex flex-column h-100">
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MW4WZ9N" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- Back to top button -->
     <a href="javascript:" id="return-to-top"><i class="icon-chevron-up"></i></a>
-    <header>
-        <nav id="menu-nav" class="navbar navbar-expand-lg navbar-dark fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="/">
-                    <img src="/img/logoBleu.png" height="30" class="d-inline-block align-top" alt="Logo Kévin RIZZO">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item px-3<?php if (isset($navbar_section) && $navbar_section == "index") echo ' active'; ?>">
-                            <a class="nav-link" href="/">
-                                Accueil
-                                <?php if (isset($navbar_section) && $navbar_section == "index") echo ' <span class="sr-only">(current)</span>'; ?>
-                            </a>
-                        </li>
-                        <li class="nav-item px-3<?php if (isset($navbar_section) && $navbar_section == "competences") echo ' active'; ?>">
-                            <a class="nav-link" href="/pages/competences.php">
-                                Compétences
-                                <?php if (isset($navbar_section) && $navbar_section == "competences") echo ' <span class="sr-only">(current)</span>'; ?>
-                            </a>
-                        </li>
-                        <li class="nav-item px-3<?php if (isset($navbar_section) && $navbar_section == "projets") echo ' active'; ?>">
-                            <a class="nav-link" href="/pages/projets.php">
-                                Projets
-                                <?php if (isset($navbar_section) && $navbar_section == "projets") echo ' <span class="sr-only">(current)</span>'; ?>
-                            </a>
-                        </li>
-                        <li class="nav-item px-3<?php if (isset($navbar_section) && $navbar_section == "veilles") echo ' active'; ?>">
-                            <a class="nav-link" href="/pages/veilles.php">
-                                Veilles
-                                <?php if (isset($navbar_section) && $navbar_section == "veilles") echo ' <span class="sr-only">(current)</span>'; ?>
-                            </a>
-                        </li>
-                        <li class="nav-item px-3<?php if (isset($navbar_section) && $navbar_section == "contact") echo ' active'; ?>">
-                            <a class="nav-link" href="/pages/contact.php">
-                                Contact
-                                <?php if (isset($navbar_section) && $navbar_section == "contact") echo ' <span class="sr-only">(current)</span>'; ?>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
     <main role="main" class="flex-shrink-0">
--- index.php
+++ index.php
@@ -1,27 +1,125 @@
 <?php
-$page_title = "Portfolio de Kévin RIZZO | BTS SIO option SLAM";
-$page_description = "Kévin RIZZO - Étudiant en BTS SIO SLAM";
+$page_title = "Kévin RIZZO";
+$page_description = "Développement web | Sites de e-commerce";
 $navbar_section = 'index';
 require 'inc/header.php';
 ?>
 <div id="page-top-section" class="p-3 p-md-5 text-center">
-	<h1 class="display-4 font-weight-normal text-white">Portfolio de Kévin RIZZO</h1>
-	<h2 class="lead font-weight-normal text-white">BTS SIO option SLAM</h2>
+	<h1 class="display-4 font-weight-normal text-white">Kévin RIZZO</h1>
+	<h2 class="lead font-weight-normal text-white">Développement web | Création de sites de e-commerce</h2>
 </div>
-<div class="container">
+<div class="container my-5">
 	<div class="row">
 		<div class="col-lg-9 m-auto">
-			<div class="my-4 text-justify">
-				<p class="h4 mb-4">Bienvenue,</p>
-				<p>Je m'appelle Kévin RIZZO, je suis candidat à l'examen du BTS SIO option SLAM : Services Informatiques aux Organisations option Solutions Logicielles et Applications Métiers.</p>
-				<p>Je me présente à l’examen du BTS au titre de mon expérience professionnelle de 4 ans, en tant que développeur web, au sein de l’entreprise Via Sante. J’ai également suivi 2 années d’études par correspondance, via le CNED.</p>
-				<p>Par la suite, je souhaiterais créer une auto-entreprise afin de proposer mes services dans le domaine du développement web, plus particulièrement dans le back-end (développement côté serveur).</p>
+			<div class="mb-4 text-center">
+				<h3>Diplômes</h3>
 			</div>
-			<hr class="mt-5 mb-4">
-			<div class="my-4 text-center">
-				<a href="/pages/cv.php" target="_blank">
-					<p class="h4 mb-3">Voir mon C.V.</p>
-				</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-9 m-auto text-center">
+			<p class="h5 font-weight-light my-4">BTS SIO SLAM : Services Informatiques aux Organisations option Solutions Logicielles et Applications Métiers</p>
+			<p class="h5 font-weight-light my-4">Baccalauréat économique et social</p>
+		</div>
+	</div>
+	<hr class="mt-5">
+</div>
+<div class="container my-5">
+	<div class="row">
+		<div class="col-lg-9 m-auto">
+			<div class="mb-4 text-center">
+				<h3>Expérience professionnelle</h3>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-9 m-auto text-center">
+			<p class="h5 font-weight-light my-4">Création et gestion de sites de e-commerce <span class="font-weight-normal">@ViaSante</span> de 2015 à 2020</p>
+			<p class="h5 font-weight-light my-4">Création de site vitrine <span class="font-weight-normal">@TRcommunication</span> 2019</p>
+			<p class="h5 font-weight-light my-4">Configuration et installation de PC <span class="font-weight-normal">@Com6</span> en 2014</p>
+		</div>
+	</div>
+	<hr class="mt-5">
+</div>
+<div id="sites-web" class="container my-5">
+	<div class="row">
+		<div class="col-lg-9 m-auto">
+			<div class="mb-5 text-center">
+				<h3>Sites web créés</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row row-cols-1 row-cols-md-2 text-center">
+		<div class="col mb-4">
+			<div class="card">
+				<div>
+					<a href="https://www.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+						<img src="/img/projets/ssb-b2c.jpg" class="d-block m-auto py-4" alt="SSB - B2C">
+					</a>
+				</div>
+				<div class="card-footer border-top">
+					<h3 class="h5 card-title">Sur le Sentier des Bergers - B2C</h3>
+					<p class="card-text">Épicerie fine en ligne destinée aux particuliers.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+					<div class="mb-4 m-auto text-center">
+						<a href="https://www.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+							<button type="button" class="btn btn-primary">Voir ce site</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+				<div>
+					<a href="https://pro.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+						<img src="/img/projets/ssb-b2b.jpg" class="d-block m-auto py-2" alt="SSB - B2B" width="174">
+					</a>
+				</div>
+				<div class="card-footer border-top">
+					<h3 class="h5 card-title">Sur le Sentier des Bergers - B2B</h3>
+					<p class="card-text">Épicerie fine en ligne destinée aux professionnels.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+					<div class="mb-4 m-auto text-center">
+						<a href="https://pro.surlesentierdesbergers.fr/fr/" target="_blank" rel="noopener noreferrer">
+							<button type="button" class="btn btn-primary">Voir ce site</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+				<div>
+					<a href="https://www.laboutiquedesjambes.com/" target="_blank" rel="noopener noreferrer">
+						<img src="/img/projets/bj.jpg" class="d-block m-auto py-4" alt="BJ">
+					</a>
+				</div>
+				<div class="card-footer border-top">
+					<h3 class="h5 card-title">La Boutique des Jambes</h3>
+					<p class="card-text">Vente de produits de bien-être pour les jambes.<br>Site de e-commerce créé avec le CMS Prestashop.</p>
+					<div class="mb-4 m-auto text-center">
+						<a href="https://www.laboutiquedesjambes.com/" target="_blank" rel="noopener noreferrer">
+							<button type="button" class="btn btn-primary">Voir ce site</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card">
+				<div>
+					<a href="https://tr-communication.fr/" target="_blank" rel="noopener noreferrer">
+						<img src="/img/projets/trcom.png" class="d-block m-auto py-4" alt="TR com">
+					</a>
+				</div>
+				<div class="card-footer border-top">
+					<h3 class="h5 card-title">TR Communication</h3>
+					<p class="card-text">Community Management, Imprimerie & Signalétique.<br>Site vitrine créé avec le CMS Wordpress.</p>
+					<div class="mb-4 m-auto text-center">
+						<a href="https://tr-communication.fr/" target="_blank" rel="noopener noreferrer">
+							<button type="button" class="btn btn-primary">Voir ce site</button>
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
